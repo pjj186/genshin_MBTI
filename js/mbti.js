16f3de05afb3234fb9286dcce39ee00348869db9
Jong jae Park <pjj186@naver.com>
2021-08-15 03:20:01 +0900

    성격 유형 설명 업데이트

--- js/mbti.js
+++ js/mbti.js
@@ -234,8 +234,7 @@ const showLumine = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ISTP! 과묵하고 분석적이며, 적응력이 강한성격이래! 내 가장 친한 친구 여행자랑 똑 닮았구나?";
-  characterMBTI.className = "characterMBTI";
+    "너의 성격유형은 ISTP! 조용하고 말이 없으며 논리적이면서 분석적이고, 객관적으로 인생을 관찰하는 유형이야! 사실적인 정보를 조직하기 좋아하는 반면, 일과 관계 되지 않는 이상 자신을 잘 개방하지 않는 편이야. 그래서 가까운 친구들 외에는 대체로 사람들과 잘 사귀지 않는편이지~ 일상생활에 있어 적응력이 매우 강한타입!";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/lumine_avatar.png`;
   characterAvater.className = "avatar";
@@ -274,7 +273,7 @@ const showRazor = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ISFP! 온화하고 겸손함! 삶의 여유를 만끽하는 성격! 레이저가 생각나네~";
+    "너의 성격유형은 ISFP! 말보다는 행동으로 따뜻함을 나타내며, 마음이 따뜻하고 동정적인 성격이야. 마치 양털안감을 넣은 오버코트처럼 속마음이 따뜻한 사람들이 지닌 성격 유형이지~ 그러나 상대방을 잘 알게 될 때까지 이 따뜻함을 드러내지 않는 경향이있어!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/razor_avatar.png`;
@@ -316,7 +315,7 @@ const showSucrose = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 INTP! 지적 호기심이 높으며, 잠재력과 가능성을 중요시하는 성격이래! 널 보니까 설탕이 생각나!";
+    "너의 성격유형은 INTP! 조용하고 과묵하나 관심이 있는 분야에 대해서는 정말 말을 잘해! 사람들을 중심으로 한 가치보다는 아이디어에 관심이 많으며 매우 분석적이고 객관적 비평을 잘해! 실체보다는 실체가 안고있는 가능성에 관심이 많아! 또 이해가 빠르고 높은 직관력으로 통찰하는 재능과 지적관심이 많은편이야!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/sucrose_avatar.png`;
@@ -357,7 +356,7 @@ const showMona = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 INTJ! 의지가 강하고, 독립적이고, 분석력이 뛰어난 성격! 모나가 생각나! 너.. 굶고 다니는건 아니지?!";
+    "너의 성격유형은 INTJ! 행동과 사고에 있어서 독창적이야! 내적인 신념과 비전은 산이라도 움직일 만큼 강한편! 16가지 성격 유형중에서 가장 독립적이고 단호한 성격이야! 때때로, 어떤 문제에 대하여 아주 고집이 센 편이기도해!자신이 가진 영감과 목적을 실현시키려는 의지와 결단력과 인내심을 가지고 있어! 자신과 타인의 능력을 중요시하며, 목적달성을 위하여 온 시간과 노력을 다 바쳐 일하기 때문에 직관력과 통찰력이 활용되는 분야에서 능력을 발휘하는 편이야!.";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/mona_avatar.png`;
@@ -398,7 +397,7 @@ const showFischl = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 INFP! 성실하고 이해심 많으며, 개방적인 성격이야. 잘 표현하지 않으나, 내적 신념이 강한 성격! 피슬.. 잘 지내고 있겠지?";
+    "너의 성격유형은 INFP! 마음이 따뜻하나, 상대방을 잘 알기 전에는 표현을 잘하지 않는성격이야! 또, 자신이 관계하는 사람이나 일에 대하여 강하고 성실하지! 또한 자신이 지향하는 이상에 대하여는 정열적인 신념을 가지고 있어!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/fischl_avatar.png`;
@@ -440,7 +439,7 @@ const showXingqiu = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 INFJ! 높은 통찰력으로 사람들에게 영감을 주는 타입이야. 또, 공동체의 이익을 중요시하게 생각해. 행추가 생각나네!";
+    "너의 성격유형은 INFJ! 창의력과 통찰력이 뛰어나고, 강한 직관력으로 의미와 진실된 관계를 추구하는 타입이야! 뛰어난 영감으로 말없이 타인에게 영향력을 끼치기도해! 독창성과 사적인 독립심이 강하고, 확고한 신념과 뚜렷한 원리원칙을 생활 속에 가지고 있으면서 공동의 이익을 가져오는 일에 심혈을 기울이는 편이야! 인화와 동료애를 중시하는 경향으로 존경을 받고 사람들이 따르는 타입! 열정과 신념으로 자신들이 믿는 영감을 구현시켜 나갈 때 위대한 정신적 지도자들이 이 유형에서 많이 나와! 남에게 강요하기보다는 행동과 권유로 사람들의 마음을 움직여 따르게 만드는 지도력이 있어!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/xingqiu_avatar.png`;
@@ -481,7 +480,7 @@ const showXiao = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ISTJ! 책임감이 강하며, 현실적인 타입! 매사에 철저하고 보수적이다! 소랑 닮았어!";
+    "너의 성격유형은 ISTJ! 매우 신뢰성이 있고, 사실에 대해 완전하고, 현실적이고, 실용적인 면을 가지고있어! 어떠한 분량의 사실이라도 몰두하고, 기억하며, 이용하고, 정확도에 대해 매우 세심해! 위기상황에 대처할 때에도 차분하며 안정되어 있어! 외면적으로 볼때 차분하게 보이지만, 내면적으로는 상당히 바빠!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/xiao_avatar.png`;
@@ -521,7 +520,7 @@ const showJean = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ISFJ! 차분하고 헌신적이며, 인내심이 강한 타입! 타인의 감정변화에 주의를 기울인다! 진 단장같은 성격이구나!";
+    "너의 성격유형은 ISFJ! 책임감이 강하고 온정적이며 헌신적이야! 세부적이고 치밀성과 반복을 요하는 일을 끝까지 해나가는 인내력이 높아! 가지고 있는 침착성과 인내력은 가정이나 집단에 안정성을 가져다주지! 다른 사람의 사정을 고려하여 자신과 타인의 감정흐름에 민감해! 일처리에 있어서 현실감각을 가지고 실제적이고 조직적으로 이행하지! 경험을 통해서 자기가 틀렸다고 인정하기까지 어떠한 난관이 있어도 꾸준히 밀고 나가는 유형이야! 온순하고 차분해!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/jean_avatar.png`;
@@ -561,7 +560,7 @@ const showChilde = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ESTP! 느긋하고, 관용적이며, 타협을 잘한다. 현실적 문제 해결에 능숙한 타입! 너도 혹시 장난감 판매원이야?";
+    "너의 성격유형은 ESTP! 관대하고 느긋하며 어떤 사람이나 사건에 대해서 별로 선입견을 갖지 않으며 개방적인 성격이야! 자신에게나 타인에게 있어 관용적이며, 일을 있는 그대로 보고 받아들이지! 그래서 갈등이나 긴장이 일어나는 상황을 잘 무마하는 능력이 있어! 규범을 적용하기보다는 누구나 만족할 수 있는 해결책을 모색하고 타협하고 적응하는 능력이 있어! 주의집중을 현재 상황에 맞추고, 현실을 있는 그대로 보는 이 유형의 자연스러운 경향으로 인해 현실적으로 야기되는 문제 해결에 뛰어난 능력을 발휘하기도 해!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/childe_avatar.png`;
@@ -601,7 +600,7 @@ const showClee = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ESFP! 호기심이 많으며, 개방적인 타입! 구체적인 사실을 중시한다! 너.. 혹시 폭발물에도 호기심이 있는건 아니지?!";
+    "너의 성격유형은 ESFP! 친절하고 수용적이며 현실적이고 실제적인 성격이야! 어떤 상황에도 잘 적응하며 타협적이고, 선입견이 별로 없으며 개방적이고 관용적이라서 대체로 사람들을 잘 받아들이는 타입이야! 주위에서 진행되고 있는 일들을 알고자 하고 또한 열심히 참견하고 끼어드는 타입이야! 다른 사람의 일이나 활동에 관심이 많고, 새로운 사건 혹은 물건에도 호기심이 많이 때문이야! 이론이나 책을 통해 배우기보다는 경험 등의 실생활을 통해 배우는 것을 좋아해!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/clee_avatar.png`;
@@ -642,7 +641,7 @@ const showVenti = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ENTP! 박학다식하고, 독창적이다! 끊임없이 새로운 시도를 하는타입! 벤티가 생각나!";
+    "너의 성격유형은 ENTP! 독창적인 혁신가이며, 창의력이 풍부하고 항상 새로운 가능성을 찾고 새로운 시도를 즐기는 유형이야! 넓은 안목을 가지고 있으며 다방면에 재능이 있지! 민첩하고 여러 가지 일에 재능을 발휘하며 자신감이 넘쳐나! 사람들의 동향에 대해 기민하고 박식한 편! 다른 사람을 판단하기보다 이해하려고 노력해!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/venti_avatar.png`;
@@ -683,7 +682,7 @@ const showKeqing = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ENTJ! 철저한 준비를 하며, 활동적이다! 통솔력이 있으며, 단호하다! 리월 칠성의 옥형성이랑 비슷한 성격이구나!";
+    "너의 성격유형은 ENTJ! 활동적이며 행정적인 일과 장기계획을 선호하며 논리적이고 분석적이야! 사전 준비를 철저하게 하며 계획하고 조직하고 체계적으로 목적달성을 추진시키는 지도자들이 많아! 비능률적이거나 확실치 않는 상황에 대해서는 별로 인내심이 없어! 그러나 상황이 필요로 할 때는 강하게 대처해! 솔직하고 결정력과 통솔력이 있으며, 거시적 안목으로 일을 밀고 나가! 관념 자체에 집중하는 경향이 있으며, 관념 이면의 사람에게는 관심이 별로 없어!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/keqing_avatar.png`;
@@ -723,7 +722,7 @@ const showHutao = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ENFP! 상상력이 풍부하고, 순발력이 뛰어나다! 일상적인 활동에 지루함을 느낀다! 호두랑 비슷한 타입인가?!";
+    "너의 성격유형은 ENFP! 열성적이고 창의적이야! 풍부한 상상력과 영감을 가지고 어떤 일을 시작해! 풍부한 충동적 에너지를 가지고 죽흥적으로 일을 재빠르게 해결하는 솔선수범력과 상상력이 있어! 관심이 있는 일이면 무엇이든 척척 해내는 열성파! 뛰어난 통찰력으로 그 사람 안에 있는 성장 발전할 가능성을 들여다봐!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/hutao_avatar.png`;
@@ -763,7 +762,7 @@ const showBabara = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ENFJ! 사교적이고, 타인의 의견을 존중한다! 비판을 받으면 예민하게 반응한다! 바바라랑 비슷한 성격이구나!";
+    "너의 성격유형은 ENFJ! 동정심과 동료애가 많으며, 친절하고 재치있는 성격이야! 민첩하고 참을성이 많고 성실해! 사람들과의 인화를 중요시 여기며, 다른사람의 의견을 존중하고 그 가치를 보는 타입! 공동의 선을 위해서 대체로 상대방 의견에 동의하는 경향이 있어! 새로운 아이디어에 대한 호기심이 많아! 쓰기보다는 말로써 생각을 잘 표현하고, 편안하고 능란하게 계획을 제시하거나 조직을 이끌어나가는 능력이 있어!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/babara_avatar.png`;
@@ -804,7 +803,7 @@ const showNingguang = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ESTJ! 체계적으로 일하고, 규칙을 준수하는걸 선호해! 그리고, 사실적 목표 설정에 능하지! 리월 칠성의 천권.. 응광이랑 비슷한 성격이야!";
+    "너의 성격유형은 ESTJ! 일을 조직하고 프로젝트를 계획하고 출범시키는 능력이 뛰어나! 현실적이고 사실적이며, 체계적, 논리적으로 사업이나 조직체를 이끌어 나가는 타고난 재능을 가졌어! 혼돈스러운 상태나 불분명한 상태 또는 실용성이 없는 분야에는 큰 흥미가 없어! 그러나 필요시에는 언제나 응용하는 힘이 있어! 분명한 규칙을 중요시하고 그에 따라 행동하고 일을 추진하고 완성해 나가! 어떤 계획이나 결정을 내릴 땐 확고한 사실에 바탕을 두고 이행해!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/ningguang_avatar.png`;
@@ -845,7 +844,7 @@ const showAmber = () => {
   characterMent.className = "characterMent";
   const characterMBTI = document.createElement("span");
   characterMBTI.innerText =
-    "너의 성격유형은 ESFJ! 사람에 대한 관심이 많으며, 친절하다. 동정심이 많다! 내 친구 엠버랑 판박이네~";
+    "너의 성격유형은 ESFJ! 동정심과 동료애가 많은 성격이야! 친절하고 재치가 있으며, 다른 사람들에게 관심을 쏟고 인화를 도모하는 일을 중요하게 여겨! 양심적이고 정리정돈을 잘하지! 참을성이 많고 다른 사람들을 잘 도와줘! 또한 다른 사람들의 지지를 받으면 일에 열중해! 그러나 다른 사람들의 무관심한 태도에는 민감해!";
   characterMBTI.className = "characterMBTI";
   const characterAvater = document.createElement("img");
   characterAvater.src = `./images/amber_avatar.png`;
